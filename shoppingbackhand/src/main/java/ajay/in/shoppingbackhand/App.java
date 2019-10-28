84bb5dd1df289d0b0ebb6cc49625cafe124ebb19
Ajinkya <patil28ajinkya@gmail.com>
2019-10-28 17:59:50 +0530

    MY PROJECT SETUP COMPLETED SUCCESSFUY

---
+++ shoppingbackhand/src/main/java/ajay/in/shoppingbackhand/App.java
@@ -0,0 +1,13 @@
+package ajay.in.shoppingbackhand;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ shoppingbackhand/src/test/java/ajay/in/shoppingbackhand/AppTest.java
@@ -0,0 +1,20 @@
+package ajay.in.shoppingbackhand;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
